380dc31f21d05e2f5a1e1b9d3da039a412208332
Mikhail <57192599+luntovskymikhail@users.noreply.github.com>
2020-05-02 13:23:21 +0300

    111

--- Browser.py
+++ Browser.py
@@ -1,14 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome(executable_path="C:\Drivers\chromedriver_win32\chromedriver.exe")
+driver = webdriver.Chrome(executable_path="C:\Drivers\chromedriver.exe")
 driver.get("https://yandex.ru/")
 name = "Яндекс"
 if driver.title == name:
     print("Pass - Заголовок")
 else:
     print("Fail - Заголовок")
-    print("ssad")
-
 driver.close()
-#comment
