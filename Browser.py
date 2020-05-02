29cded9df5903377b62c1d873a44b07f21f47c3e
Mikhail <57192599+luntovskymikhail@users.noreply.github.com>
2020-05-02 13:52:41 +0300

    added ff and ie

--- Browser.py
+++ Browser.py
@@ -1,11 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome(executable_path="C:\Drivers\chromedriver.exe")
-driver.get("https://yandex.ru/")
+driverc = webdriver.Chrome(executable_path="C:\Drivers\chromedriver.exe")
+driverf = webdriver.Firefox(executable_path="C:\Drivers\geckodriver.exe")
+driverie = webdriver.Ie(executable_path="C:\Drivers\IEDriverServer.exe")
+
+#chrome
+driverc.get("https://yandex.ru/")
 name = "Яндекс"
-if driver.title == name:
-    print("Pass - Заголовок")
+if driverc.title == name:
+    print("Pass - Заголовок Chrome")
 else:
-    print("Fail - Заголовок")
-driver.close()
+    print("Fail - Заголовок Chrome")
+driverc.close()
+
+#firefox
+driverf.get("https://yandex.ru/")
+name = "Яндекс"
+if driverf.title == name:
+    print("Pass - Заголовок Firefox")
+else:
+    print("Fail - Заголовок Firefox")
+driverf.close()
+
+#internet explorer
+driverie.get("https://yandex.ru/")
+name = "Яндекс"
+if driverie.title == name:
+    print("Pass - Заголовок IE")
+else:
+    print("Fail - Заголовок IE")
+driverie.close()
