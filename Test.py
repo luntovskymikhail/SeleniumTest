c6acca799d5c1b66d03e78ea41586857a25622f9
Mikhail <57192599+luntovskymikhail@users.noreply.github.com>
2020-05-03 15:04:58 +0300

    back, forward, sleep

--- Test.py
+++ Test.py
@@ -2,12 +2,22 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-driverchrome = webdriver.Chrome(executable_path="C:\Drivers\chromedriver.exe")
+driver = webdriver.Chrome(executable_path="C:\Drivers\operadriver.exe")
+def wait():
+    time.sleep(13)
 
-driverchrome.get("https://vk.com/")
+#open main page
+driver.get("https://www.spbrealty.ru/")
+wait()
 
-driverchrome.find_element_by_xpath('//*[@id="index_login_button"]').click()
+#goes to about company
+driver.find_element_by_xpath('//*[@id="emerge-header"]/div[3]/div/div/div[1]/ul/li[4]/a').click()
+wait()
 
-time.sleep(5)
+driver.back()
+wait()
 
-driverchrome.quit()
+driver.forward()
+wait()
+
+driver.quit()
